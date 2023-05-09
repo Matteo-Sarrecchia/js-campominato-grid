8d6535ef1665c76edd99b416cad7a8e6bc9a8b44
Matteo Sarrecchia <morrea@hotmail.it>
2023-05-09 19:21:14 +0200

    bonus

--- js/main.js
+++ js/main.js
@@ -1,40 +1,91 @@
 const container = document.getElementById("container");
+const select = document.getElementById("select")
 const buttonPlay = document.getElementById("button-play")
 
 buttonPlay.addEventListener("click",
     
     function(){
-        for (let i=1; i <= 100; i++ ){
-            const square = createElement("div", "square")
-            console.log(square);
-        
-            container.append(square)
-        
-            // creo elemento span per insierire i numeri
-            const span = createElement("span", "span")
-            square.append(span)
-            span.innerHTML = i
-        
-            // aggiungi a square evento con click
-            square.addEventListener("click",
+
+        container.innerHTML = "";
+
+        if (select.value === "Easy"){
+            for (let i=1; i <= 49; i++ ){
+                const square = createElement("div", "square-easy")
             
-                function(){
-                    square.classList.add("blue")
-                    console.log("Hai cliccato la casella numero: ", i)
-                }
-        
-            )
-        
+                container.append(square)
+            
+                // creo elemento span per inserire i numeri
+                const span = createElement("span", "span")
+                square.append(span)
+                span.innerHTML = i
+            
+                // aggiungi a square evento con click
+                square.addEventListener("click",
+                
+                    function(){
+                        square.classList.add("blue")
+                        console.log("Hai cliccato la casella numero: ", i)
+                    }
+            
+                )
+            
+            }
+
+        }if (select.value === "Normal"){
+            for (let i=1; i <= 81; i++ ){
+                const square = createElement("div", "square-normal")
+            
+                container.append(square)
+            
+                // creo elemento span per inserire i numeri
+                const span = createElement("span", "span")
+                square.append(span)
+                span.innerHTML = i
+            
+                // aggiungi a square evento con click
+                square.addEventListener("click",
+                
+                    function(){
+                        square.classList.add("blue")
+                        console.log("Hai cliccato la casella numero: ", i)
+                    }
+            
+                )
+            
+            }
+            
+
+        }if (select.value === "Hard"){
+            for (let i=1; i <= 100; i++ ){
+                const square = createElement("div", "square-hard")
+            
+                container.append(square)
+            
+                // creo elemento span per inserire i numeri
+                const span = createElement("span", "span")
+                square.append(span)
+                span.innerHTML = i
+            
+                // aggiungi a square evento con click
+                square.addEventListener("click",
+                
+                    function(){
+                        square.classList.add("blue")
+                        console.log("Hai cliccato la casella numero: ", i)
+                    }
+            
+                )
+            
+            }
         }
-}
+        
+        
+    }
 
 )
 
 
-
-
-addEventListener
-
+console.log(select.value)
 
 // FUNZIONI
 function createElement (tag, nameClassAdd){
