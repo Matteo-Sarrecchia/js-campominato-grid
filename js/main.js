7cf7abfe4d6a84400592316c7b254c3ff22da59c
Matteo Sarrecchia <morrea@hotmail.it>
2023-05-09 17:16:44 +0200

    struttura e funzioni base

---
+++ js/main.js
@@ -0,0 +1,35 @@
+const container = document.getElementById("container");
+
+for (let i=1; i <= 100; i++ ){
+    const square = createElement("div", "square")
+    console.log(square);
+
+    container.append(square)
+
+    // creo elemento span per insierire i numeri
+    const span = createElement("span", "span")
+    square.append(span)
+    span.innerHTML = i
+
+    // aggiungi a square evento con click
+    square.addEventListener("click",
+    
+        function(){
+            square.classList.add("blue")
+            console.log("Hai cliccato la casella numero: ", i)
+        }
+
+    )
+
+}
+
+
+addEventListener
+
+
+// FUNZIONI
+function createElement (tag, nameClassAdd){
+    const newElement = document.createElement(tag);
+    newElement.classList.add(nameClassAdd)
+    return newElement
+}
